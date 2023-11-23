7543c69c0cf835e609466e532c6bbe23deaf13f5
P KIRAN <147041511+pkiran-exe@users.noreply.github.com>
2023-11-23 15:20:21 +0530

    Add files via upload

---
+++ Data/SchoolDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PracticeSec4.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public SchoolDbContext() : base("name=SchoolDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<PracticeSec4.Models.Classes> Classes { get; set; }
+
+        public System.Data.Entity.DbSet<PracticeSec4.Models.Student> Students { get; set; }
+
+        public System.Data.Entity.DbSet<PracticeSec4.Models.Subject> Subjects { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace PracticeSec4.Models
+{
+    [Table("StTable")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        public int ClassId { get; set; }
+        public virtual ICollection<Classes> Classes { get; set; }
+        public virtual ICollection<Subject> Subject { get; set; }
+    }
+}
